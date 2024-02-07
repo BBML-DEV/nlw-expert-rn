88a8f38ed2081da247bbd09d86f135e83a12f802
Paulo Vitor <pv.marques2010@hotmail.com>
2024-02-07 19:35:10 -0300

    feat: Estrutura inicial do projeto

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from "react-native"
+
+
+export default function Home(){
+
+    return (
+        <View className="flex-1 pt-8">
+            <Text className="text-orange-400 text-2xl">
+                Hello, world
+            </Text>
+        </View>
+    )
+}
