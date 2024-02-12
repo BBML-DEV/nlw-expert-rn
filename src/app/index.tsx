be43d3c34dae25fb3e79a9faeb94b0aa8e0e2843
Paulo Vitor <pv.marques2010@hotmail.com>
2024-02-12 15:53:10 -0300

    feat: Finalizando a lista de produtos da tela inicial do app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,32 @@
 import CategoryButton from "@/components/categoryButton"
 import Header from "@/components/header"
-import {View, FlatList} from "react-native"
-import { CATEGORIES } from "@/utils/data/products"
-import { useState } from "react"
+import  Product  from "@/components/product"
+import {View, FlatList, SectionList, Text} from "react-native"
+import { CATEGORIES, MENU} from "@/utils/data/products"
+import { useState, useRef} from "react"
+
 
 
 export default function Home(){
 
     const [category, setCategory] = useState(CATEGORIES[0])
 
+    const sectionListRef = useRef<SectionList>(null)
+
     const handleCategorySelect = (selectedCategory: string) => {
         setCategory(selectedCategory)
+
+        const sectionIndex = CATEGORIES.findIndex((category) => category === selectedCategory)
+
+
+        if(sectionListRef.current){
+            sectionListRef.current.scrollToLocation({
+                animated: true,
+                sectionIndex,
+                itemIndex: 0
+            })
+        }
+        console.log(sectionIndex)
     }
 
     return (
@@ -26,7 +42,21 @@ export default function Home(){
                 horizontal
                 className="max-w-10 mt-5"
                 showsHorizontalScrollIndicator={false}
-                contentContainerStyle={ { gap: 12 , paddingHorizontal: 20} }
+                contentContainerStyle={ { gap: 12 , paddingHorizontal: 20, marginBottom: 20} }
+            />
+
+            <SectionList
+                ref={sectionListRef}
+                sections={MENU}
+                keyExtractor={(item) => item.id}
+                stickySectionHeadersEnabled={false}
+                renderItem={({item}) => <Product data={item} />}
+                renderSectionHeader={({section : {title} }) => (
+                    <Text className="text-xl text-white font-heading mt-8 mb-3">{title}</Text>
+                )}
+                className="p-5"
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={ {paddingBottom: 100} } 
             />
         </View>
     )
